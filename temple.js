4f0b86d8886bdbc0efb86ca5fa2563c455eb7bda
blaqsnow001 <ssnowsoft1@gmail.com>
2026-03-12 02:35:17 +0100

    Add initial HTML structure, CSS styles, and JavaScript functionality for temple album

---
+++ temple.js
@@ -0,0 +1,10 @@
+document.getElementById("last-modified").textContent = "Last Modified: " + document.lastModified;
+
+document.getElementById("hamburger").addEventListener('click', () => {
+    document.querySelector("nav").classList.add("open");
+});
+
+document.getElementById("close-btn").addEventListener('click', () => {
+    document.querySelector("nav").classList.remove("open");
+});
+
